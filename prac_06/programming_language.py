106364aa0bc29ad14ab731a47073b50e169996a7
yunseo choi <160458927+zzzczzzz@users.noreply.github.com>
2024-03-22 23:07:40 +0800

    programming language.py
    test language class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,22 @@
+"""
+estimation of time taken : 40mins
+start at 1:22
+ended at 1:45
+actually took 23mins less than expected
+"""
+class ProgrammingLanguage:
+    """init function contain name typing reflection and their year"""
+    def __init__(self, language_name, typing, reflection, year):
+        self.language_name = language_name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """determine whether dynamic or not"""
+        return self.typing.title() == 'Dynamic'
+
+    def __str__(self):
+        """return string of all information"""
+        return f"{self.language_name}, {self.typing}, Reflection={self.reflection}, First appeared in {self.year}"
+
